2c72158abaa45f0f3c9a1b5f6b3e55e142788ef6
UlysC <ulys.chambard@gmail.com>
2021-03-04 15:50:36 +0100

    fragment meta tag

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,7 +12,7 @@ export default {
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
       { hid: "description", name: "description", content: "" },
-      { name: "fragment", content: "!" }
+      // { name: "fragment", content: "!" }
     ],
     link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
