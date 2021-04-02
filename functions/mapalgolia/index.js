bb37d742a7d822d96209a41aa3b6f6748e0e9f54
UlysC <ulys.chambard@gmail.com>
2021-04-02 14:19:25 +0200

    mapalgolia with MD5s

--- functions/mapalgolia/index.js
+++ functions/mapalgolia/index.js
@@ -24,7 +24,7 @@ exports.handler = async (event, context) => {
     return {
       statusCode: 200,
       headers,
-      body: JSON.stringify(response),
+      body: response.r,
     }
   } catch (e) {
     return {
