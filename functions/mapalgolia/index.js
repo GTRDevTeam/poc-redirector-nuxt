69115c2a189e24767c486a5fe06922d01760a57c
UlysC <ulys.chambard@gmail.com>
2021-04-02 14:18:32 +0200

    mapalgolia with MD5s

--- functions/mapalgolia/index.js
+++ functions/mapalgolia/index.js
@@ -11,27 +11,26 @@ const ALGOLIA_APP_ID = "F1XZFUDSPJ"
 const ALGOLIA_API_KEY = "fb71ffe2a825e195b385aeb276330b9a"
 
 exports.handler = async (event, context) => {
-  const from = event.headers.referer.split(`${event.headers.host}/`)[1]
-  const instance = event.headers.host.split(".")[0]
   try {
+    const from = event.headers.referer.split(`${event.headers.host}/`)[1]
+    const instance = event.headers.host.split(".")[0]
     const hashed_instance = crypto.createHash("md5").update(instance).digest("hex")
+    const hashed_from = crypto.createHash("md5").update(from).digest("hex")
+    console.log({from, instance, hashed_from, hashed_instance})
     const ALGOLIA_CLIENT = algoliasearch(ALGOLIA_APP_ID, ALGOLIA_API_KEY)
     const INDEX = ALGOLIA_CLIENT.initIndex(`redirect_${hashed_instance}`)
-    const hashed_from = crypto.createHash("md5").update(from).digest("hex")
     response = await INDEX.getObject(hashed_from)
 
     return {
       statusCode: 200,
       headers,
-      // body: JSON.stringify(response),
-      body: JSON.stringify({from, hashed_from})
+      body: JSON.stringify(response),
     }
   } catch (e) {
     return {
       statusCode: 404,
       headers,
-      // body: e.message,
-      body: JSON.stringify({ from, hashed_from }),
+      body: e.message,
     }
   }
 }
