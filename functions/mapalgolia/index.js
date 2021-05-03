792a862eed35b4147ac4fa3013d2f867be50d9b6
UlysC <ulys.chambard@gmail.com>
2021-05-03 16:23:50 +0200

    loggly tryout

--- functions/mapalgolia/index.js
+++ functions/mapalgolia/index.js
@@ -1,5 +1,16 @@
 const algoliasearch = require("algoliasearch")
 const crypto = require("crypto")
+const winston = require("winston")
+const { Loggly } = require("winston-loggly-bulk")
+
+winston.add(
+  new Loggly({
+    token: "8843034e-85cd-469c-827b-01acc453a0c9",
+    subdomain: "gtrtest",
+    tags: ["Winston-NodeJS"],
+    json: true,
+  })
+)
 
 const headers = {
   "Access-Control-Allow-Origin": "*",
@@ -10,7 +21,7 @@ const headers = {
 const ALGOLIA_APP_ID = "F1XZFUDSPJ"
 const ALGOLIA_API_KEY = "fb71ffe2a825e195b385aeb276330b9a"
 
-exports.handler = async (event, context) => {
+exports.handler = async event => {
   try {
     const from = event.headers.referer.split(`${event.headers.host}/`)[1]
     const instance = event.headers.host.split(".")[0]
@@ -20,6 +31,7 @@ exports.handler = async (event, context) => {
     const INDEX = ALGOLIA_CLIENT.initIndex(`redirect_${hashed_instance}`)
     try {
       response = await INDEX.getObject(hashed_from)
+      winston.log(JSON.stringify({from, instance, response}))
       return {
         statusCode: 200,
         headers,
@@ -27,7 +39,7 @@ exports.handler = async (event, context) => {
       }
     }
     catch (e) {
-      // Object does not exist -> return client-specific fallback
+      // Object does not exist -> returns client-specific fallback
       response = await INDEX.getObject("fallback")
        return {
          statusCode: 200,
@@ -36,7 +48,7 @@ exports.handler = async (event, context) => {
        }
     }
   } catch (e) {
-    // Index does not exist or any other error -> return hard coded client-agnostic fallback
+    // Index does not exist or any other error -> returns hard coded client-agnostic fallback
     return {
       statusCode: 200,
       headers,
